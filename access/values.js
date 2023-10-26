08f808590a03bcc716c7ed019304daec5bf68857
M1shutqa <143396754+M1shutqa@users.noreply.github.com>
2023-10-26 11:43:04 +0300

    Add files via upload

---
+++ access/values.js
@@ -0,0 +1,24 @@
+// Активировать документы (true - включены, false - отключены)
+var isRightsEnabled = true; // Права
+var isCovidCertificateEnabled = false; // COVID-сертификат
+
+// Общее
+var birthdate = "13.09.2005"; // Дата рождения
+var fullname = "Дем'яненко Денис Андрійович" // ФИО
+var name = "Денис" // Имя
+
+// Паспорт
+var passport_id = "005233561" // Номер паспорта
+
+// КПП
+var kpp_id = "3892113791" // ИНН
+
+// Права
+var rights_valid_until = "16.11.2024" // Права "Дійсні до"
+var rights_categories = "A1, A" // Права "Категорії"
+var rights_tsc = "ТСЦ 0742" // Права "Видав
+var rights_id = "ВС137635" // Права "Серія та номер""
+
+// COVID-сертификат
+var covid_valid_until = "01.01.2022" // Сертификат "Дійсний до"
+var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
